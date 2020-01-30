466b55a4abd4ae1675c994cccd72038396599420
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-30 17:27:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -14,9 +14,9 @@ def crew
 end
 
 def greet_crew (crew)
-  binding.pry
+  
  
-  crew.each {|crew_member| puts "Hello #{crew_member}."}
+  crew.map {|crew_member| "Hello #{crew_member}."}
   
 end
 
