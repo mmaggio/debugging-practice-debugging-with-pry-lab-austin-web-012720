da0126fb0bc8692eb39593ac36491255834ef892
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-30 16:50:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,9 +5,9 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
   
 end
-
+binding.pry
 def play_game(player2)
   
   "#{player1} is better than #{player2}"
-  binding.pry
+  
 end
